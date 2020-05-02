73d190e02280bc7488127e4760c0b609147ee3d8
Thierri Oliveira <thierri.oliveira@gmail.com>
2020-05-02 19:09:49 -0300

    Itens

--- whatsapp/src/App.vue
+++ whatsapp/src/App.vue
@@ -4,8 +4,21 @@
       <div class="container">
         <div class="columns principal">
           <div class="column is-3 conversas">
+            <div class="top-bar"/>
+            <div class="content conversa">
+              <div class="title is-6">Usuario</div>
+              <div class="subtitle is-6">Conversa</div>
+            </div>
+            <div class="content conversa">
+              <div class="title is-6">Usuario</div>
+              <div class="subtitle is-6">Conversa</div>
+            </div>
           </div>
           <div class="column conversa-ativa">
+            <div class="top-bar"/>
+            <div class="bottom-bar">
+              <input type="text" placeholder="Insira sua mensagem">
+            </div>
             <!-- Conversa Ativa -->
           </div>
         </div>
