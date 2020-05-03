6546734de77676054addb9fe64894338f3d6f9d9
Thierri Oliveira <thierri.oliveira@gmail.com>
2020-05-03 11:40:08 -0300

    Style Pronto

--- whatsapp/src/App.vue
+++ whatsapp/src/App.vue
@@ -2,37 +2,37 @@
   <div id="app">
     <section class="section">
       <div class="container">
-        <div class="columns principal">
-          <div class="column is-3 conversas">
-            <div class="top-bar"/>
-            <div class="content conversa">
+        <div class="columns janela-principal">
+          <div class="column is-3 lista-de-conversas">
+            <div class="barra-superior"/>
+            <div class="item">
               <div class="title is-6">Usuario</div>
               <div class="subtitle is-6">Conversa</div>
             </div>
-            <div class="content conversa">
+            <div class="item">
               <div class="title is-6">Usuario</div>
               <div class="subtitle is-6">Conversa</div>
             </div>
           </div>
           <div class="column conversa-ativa">
-            <div class="top-bar">
+            <div class="barra-superior">
               <span>Usuario</span>
             </div>
-            <div class="mensagens">
-            <div class="mensagem-usuario">
-              Esta é uma mensagem do usuário de lá.
-              <span>10:15</span>
+            <div class="lista-mensagens">
+              <div class="mensagem">
+                Esta é uma mensagem do usuário de lá.
+                <span>10:15</span>
+              </div>
+              <div class="mensagem">
+                Esta é uma mensagem do usuário de lá.
+                <span>10:15</span>
+              </div>
+              <div class="mensagem verde">
+                Esta é uma mensagem do usuário de lá.
+                <span>10:15</span>
+              </div>
             </div>
-            <div class="mensagem-usuario">
-              Esta é uma mensagem do usuário de lá.
-              <span>10:15</span>
-            </div>
-            <div class="mensagem-usuario local">
-              Esta é uma mensagem do usuário de lá.
-              <span>10:15</span>
-            </div>
-            </div>
-            <div class="bottom-bar">
+            <div class="barra-inferior">
               <input type="text" class="input" placeholder="Insira sua mensagem">
             </div>
           </div>
